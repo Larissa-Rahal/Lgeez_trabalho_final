9c1b57a255d5c7ef791f33043107ec5fcde50db6
Allan <allanangelonf@gmail.com>
2024-05-02 15:38:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,339 @@
+const produtos = [
+  {
+    nome: "NeuroWave Headset",
+    descricao:
+      "Um headset revolucionário que utiliza tecnologia de ondas cerebrais para controlar dispositivos eletrônicos com a mente. Experimente uma nova forma de interação com seus dispositivos favoritos.",
+    categoria: ["Dispositivos Vestíveis", "Tecnologia de Controle Mental"],
+    preco: 299.99,
+  },
+  {
+    nome: "QuantumDrive SSD",
+    descricao:
+      "O SSD mais rápido do mercado, alimentado por tecnologia de computação quântica. Experimente tempos de carregamento ultrarrápidos e desempenho incomparável em seu computador ou console de jogos.",
+    categoria: ["Armazenamento de Dados", "Hardware de Computador"],
+    preco: 199.99,
+  },
+  {
+    nome: "EcoSmart Thermostat",
+    descricao:
+      "Um termostato inteligente que aprende seus padrões de uso e otimiza automaticamente a temperatura de sua casa para economizar energia. Controle facilmente a temperatura de qualquer lugar através de seu smartphone.",
+    categoria: ["Casa Inteligente", "Termostatos Inteligentes"],
+    preco: 129.99,
+  },
+  {
+    nome: "NeuraDrive Car Assistant",
+    descricao:
+      "Um assistente de direção avançado equipado com IA que monitora seu comportamento ao volante, oferecendo sugestões em tempo real para uma condução mais segura e eficiente.",
+    categoria: ["Tecnologia Automotiva", "Assistentes de Condução"],
+    preco: 349.99,
+  },
+  {
+    nome: "HyperSync Gaming Monitor",
+    descricao:
+      "Um monitor de jogos de alta performance com taxas de atualização ultrarrápidas e tecnologia HDR aprimorada. Desfrute de uma experiência de jogo imersiva como nunca antes.",
+    categoria: ["Monitores de Computador", "Tecnologia de Jogos"],
+    preco: 599.99,
+  },
+  {
+    nome: "NeuroWave Headset",
+    descricao:
+      "Um headset revolucionário que utiliza tecnologia de ondas cerebrais para controlar dispositivos eletrônicos com a mente. Experimente uma nova forma de interação com seus dispositivos favoritos.",
+    categoria: ["Dispositivos Vestíveis", "Tecnologia de Controle Mental"],
+    preco: 299.99,
+  },
+  {
+    nome: "QuantumDrive SSD",
+    descricao:
+      "O SSD mais rápido do mercado, alimentado por tecnologia de computação quântica. Experimente tempos de carregamento ultrarrápidos e desempenho incomparável em seu computador ou console de jogos.",
+    categoria: ["Armazenamento de Dados", "Hardware de Computador"],
+    preco: 199.99,
+  },
+  {
+    nome: "EcoSmart Thermostat",
+    descricao:
+      "Um termostato inteligente que aprende seus padrões de uso e otimiza automaticamente a temperatura de sua casa para economizar energia. Controle facilmente a temperatura de qualquer lugar através de seu smartphone.",
+    categoria: ["Casa Inteligente", "Termostatos Inteligentes"],
+    preco: 129.99,
+  },
+  {
+    nome: "NeuraDrive Car Assistant",
+    descricao:
+      "Um assistente de direção avançado equipado com IA que monitora seu comportamento ao volante, oferecendo sugestões em tempo real para uma condução mais segura e eficiente.",
+    categoria: ["Tecnologia Automotiva", "Assistentes de Condução"],
+    preco: 349.99,
+  },
+  {
+    nome: "HyperSync Gaming Monitor",
+    descricao:
+      "Um monitor de jogos de alta performance com taxas de atualização ultrarrápidas e tecnologia HDR aprimorada. Desfrute de uma experiência de jogo imersiva como nunca antes.",
+    categoria: ["Monitores de Computador", "Tecnologia de Jogos"],
+    preco: 599.99,
+  }, {
+    nome: "NeuroWave Headset",
+    descricao:
+      "Um headset revolucionário que utiliza tecnologia de ondas cerebrais para controlar dispositivos eletrônicos com a mente. Experimente uma nova forma de interação com seus dispositivos favoritos.",
+    categoria: ["Dispositivos Vestíveis", "Tecnologia de Controle Mental"],
+    preco: 299.99,
+  },
+  {
+    nome: "QuantumDrive SSD",
+    descricao:
+      "O SSD mais rápido do mercado, alimentado por tecnologia de computação quântica. Experimente tempos de carregamento ultrarrápidos e desempenho incomparável em seu computador ou console de jogos.",
+    categoria: ["Armazenamento de Dados", "Hardware de Computador"],
+    preco: 199.99,
+  },
+  {
+    nome: "EcoSmart Thermostat",
+    descricao:
+      "Um termostato inteligente que aprende seus padrões de uso e otimiza automaticamente a temperatura de sua casa para economizar energia. Controle facilmente a temperatura de qualquer lugar através de seu smartphone.",
+    categoria: ["Casa Inteligente", "Termostatos Inteligentes"],
+    preco: 129.99,
+  },
+  {
+    nome: "NeuraDrive Car Assistant",
+    descricao:
+      "Um assistente de direção avançado equipado com IA que monitora seu comportamento ao volante, oferecendo sugestões em tempo real para uma condução mais segura e eficiente.",
+    categoria: ["Tecnologia Automotiva", "Assistentes de Condução"],
+    preco: 349.99,
+  },
+  {
+    nome: "HyperSync Gaming Monitor",
+    descricao:
+      "Um monitor de jogos de alta performance com taxas de atualização ultrarrápidas e tecnologia HDR aprimorada. Desfrute de uma experiência de jogo imersiva como nunca antes.",
+    categoria: ["Monitores de Computador", "Tecnologia de Jogos"],
+    preco: 599.99,
+  }, {
+    nome: "NeuroWave Headset",
+    descricao:
+      "Um headset revolucionário que utiliza tecnologia de ondas cerebrais para controlar dispositivos eletrônicos com a mente. Experimente uma nova forma de interação com seus dispositivos favoritos.",
+    categoria: ["Dispositivos Vestíveis", "Tecnologia de Controle Mental"],
+    preco: 299.99,
+  },
+  {
+    nome: "QuantumDrive SSD",
+    descricao:
+      "O SSD mais rápido do mercado, alimentado por tecnologia de computação quântica. Experimente tempos de carregamento ultrarrápidos e desempenho incomparável em seu computador ou console de jogos.",
+    categoria: ["Armazenamento de Dados", "Hardware de Computador"],
+    preco: 199.99,
+  },
+  {
+    nome: "EcoSmart Thermostat",
+    descricao:
+      "Um termostato inteligente que aprende seus padrões de uso e otimiza automaticamente a temperatura de sua casa para economizar energia. Controle facilmente a temperatura de qualquer lugar através de seu smartphone.",
+    categoria: ["Casa Inteligente", "Termostatos Inteligentes"],
+    preco: 129.99,
+  },
+  {
+    nome: "NeuraDrive Car Assistant",
+    descricao:
+      "Um assistente de direção avançado equipado com IA que monitora seu comportamento ao volante, oferecendo sugestões em tempo real para uma condução mais segura e eficiente.",
+    categoria: ["Tecnologia Automotiva", "Assistentes de Condução"],
+    preco: 349.99,
+  },
+  {
+    nome: "HyperSync Gaming Monitor",
+    descricao:
+      "Um monitor de jogos de alta performance com taxas de atualização ultrarrápidas e tecnologia HDR aprimorada. Desfrute de uma experiência de jogo imersiva como nunca antes.",
+    categoria: ["Monitores de Computador", "Tecnologia de Jogos"],
+    preco: 599.99,
+  }, {
+    nome: "NeuroWave Headset",
+    descricao:
+      "Um headset revolucionário que utiliza tecnologia de ondas cerebrais para controlar dispositivos eletrônicos com a mente. Experimente uma nova forma de interação com seus dispositivos favoritos.",
+    categoria: ["Dispositivos Vestíveis", "Tecnologia de Controle Mental"],
+    preco: 299.99,
+  },
+  {
+    nome: "QuantumDrive SSD",
+    descricao:
+      "O SSD mais rápido do mercado, alimentado por tecnologia de computação quântica. Experimente tempos de carregamento ultrarrápidos e desempenho incomparável em seu computador ou console de jogos.",
+    categoria: ["Armazenamento de Dados", "Hardware de Computador"],
+    preco: 199.99,
+  },
+  {
+    nome: "EcoSmart Thermostat",
+    descricao:
+      "Um termostato inteligente que aprende seus padrões de uso e otimiza automaticamente a temperatura de sua casa para economizar energia. Controle facilmente a temperatura de qualquer lugar através de seu smartphone.",
+    categoria: ["Casa Inteligente", "Termostatos Inteligentes"],
+    preco: 129.99,
+  },
+  {
+    nome: "NeuraDrive Car Assistant",
+    descricao:
+      "Um assistente de direção avançado equipado com IA que monitora seu comportamento ao volante, oferecendo sugestões em tempo real para uma condução mais segura e eficiente.",
+    categoria: ["Tecnologia Automotiva", "Assistentes de Condução"],
+    preco: 349.99,
+  },
+  {
+    nome: "HyperSync Gaming Monitor",
+    descricao:
+      "Um monitor de jogos de alta performance com taxas de atualização ultrarrápidas e tecnologia HDR aprimorada. Desfrute de uma experiência de jogo imersiva como nunca antes.",
+    categoria: ["Monitores de Computador", "Tecnologia de Jogos"],
+    preco: 599.99,
+  },
+];
+
+let carrinhoDOM = [];
+
+function atualizarCarrinho() {
+  if (localStorage.getItem("carrinho")) {
+    let produtosJSON = JSON.parse(localStorage.getItem("carrinho"));
+    produtosJSON.forEach((produtin) => {
+      carrinhoDOM.push(produtin);
+    });
+  }
+}
+
+const criar = {
+  produtosBOX: () => {
+    let div = document.querySelector(".content-container");
+
+    produtos.forEach((produto) => {
+      let box = `<div class="col themed-grid-col m-1" id="produto">
+    <img src="./img/2997770089c92d82cfc9b2f2be57beb6.jpg" alt="#" />
+    <h5>${produto.nome}</h5>
+    <p>${produto.categoria}</p>
+    <h3>R$ ${produto.preco}</h3>
+    <div data-app="product.quantity">
+      <span
+        id="span_erro_carrinho"
+        class="blocoAlerta"
+        style="display: none"
+        >Selecione uma opção para variação do produto</span
+      >
+      <label for="quantidade">Quantidade:</label>
+      <input type="button" id="btn-menos" value="-" />
+      <input
+        id="quantidade"
+        name="quantidade"
+        class="text"
+        size="1"
+        type="text"
+        maxlength="5"
+      />
+      <input type="button" id="btn-mais" value="+" />
+      <button class="btn" id="btn-add">
+      <img
+        width="24"
+        height="24"
+        src="https://img.icons8.com/fluency-systems-regular/48/shopping-cart--v1.png"
+        alt="shopping-cart--v1"
+      />
+    </button>
+      </div>
+  </div>`;
+      if (div) {
+        div.innerHTML += box;
+      }
+    });
+  },
+  carrinhoBOX: () => {
+    atualizarCarrinho();
+    let div = document.querySelector(".container-carrinho");
+
+    carrinhoDOM.forEach((item) => {
+      let box = `<div>
+      <img src="./img/2997770089c92d82cfc9b2f2be57beb6.jpg" alt="#" />
+      <h5>${item.nome}</h5>
+      <p>${item.descricao}</p>
+      <p>${item.categoria}</p>
+      <h3>R$ ${item.preco}</h3>
+      <h2>${item.quantidade}</h2>
+      <h2>Total: ${+item.preço * +item.quantidade}</h2>
+      <button class="btn btn-primary" id="btn-del">
+      excluir do carrinho
+    </button>
+      </div>`;
+
+      if (div) {
+        div.innerHTML += box;
+      }
+    });
+  },
+};
+
+criar.produtosBOX();
+criar.carrinhoBOX();
+
+let carrinho = [];
+
+const items = {
+  arrays: {
+    input: Array.from(document.querySelectorAll("#quantidade")),
+    btnSo: Array.from(document.querySelectorAll("#btn-mais")),
+    btnSu: Array.from(document.querySelectorAll("#btn-menos")),
+    btnAdd: Array.from(document.querySelectorAll("#btn-add")),
+    btnDel: Array.from(document.querySelectorAll("#btn-del")),
+  },
+  inserir: () => {
+    items.arrays.btnAdd.forEach((btn, index) => {
+      let produto = Array.from(document.querySelectorAll("#produto"));
+
+      btn.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (items.arrays.input[index].value >= 1) {
+          produto[index].classList.add("ativado");
+
+          let produtoOBJ = {
+            nome: produtos[index].nome,
+            index: index,
+            descricao: produtos[index].descricao,
+            categoria: produtos[index].categoria,
+            preco: produtos[index].preco,
+            quantidade: +items.arrays.input[index].value,
+          };
+
+          localStorage.setItem("produto", JSON.stringify(produtoOBJ));
+          carrinho.push(produtoOBJ);
+          localStorage.setItem("carrinho", JSON.stringify(carrinho));
+        }
+
+        items.arrays.input[index].value = 1;
+      });
+    });
+  },
+  excluir: () => {
+    items.arrays.btnDel.forEach((btn, index) => {
+      btn.addEventListener("click", (event) => {
+        event.preventDefault();
+        let produtosJSON = JSON.parse(localStorage.getItem("carrinho"));
+        produtosJSON.splice(index, 1);
+        localStorage.setItem("carrinho", JSON.stringify(produtosJSON));
+        location.reload();
+      });
+    });
+  },
+  quantidade: {
+    aumentar: () => {
+      items.arrays.btnSo.forEach((btn, index) => {
+        items.arrays.input[index].value = 1;
+        btn.addEventListener("click", (event) => {
+          event.preventDefault();
+          items.arrays.input[index].value++;
+        });
+      });
+    },
+    diminuir: () => {
+      items.arrays.btnSu.forEach((btn, index) => {
+        items.arrays.input[index].value = 1;
+        btn.addEventListener("click", (event) => {
+          event.preventDefault();
+          if (items.arrays.input[index].value <= 1) {
+            console.log("Minimo de 1 produto.");
+          } else {
+            items.arrays.input[index].value--;
+          }
+        });
+      });
+    },
+  },
+};
+
+items.inserir();
+items.excluir();
+items.quantidade.aumentar();
+items.quantidade.diminuir();
+
+const filtragem = (filtro) => {
+  return produtos.filter((produto) => produto.categoria == filtro);
+};
